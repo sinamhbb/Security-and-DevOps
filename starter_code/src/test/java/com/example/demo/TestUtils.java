d9d78572af1e30b26cd59103b50e454b10233050
sinamhbb <sina.mhbbi@gmail.com>
2022-08-16 11:15:09 +0300

    Added Tests

---
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+
+    public static void injectObjects(Object target, String fieldName, Object toInject) {
+        boolean wasPrivate = false;
+
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+
+            if (!field.canAccess(target)) {
+                field.setAccessible(true);
+                wasPrivate = true;
+            }
+            field.set(target, toInject);
+
+            if (wasPrivate) {
+                field.setAccessible(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+
+    private UserController userController;
+
+    private UserRepository userRepository = mock(UserRepository.class);
+
+    private CartRepository cartRepository = mock(CartRepository.class);
+
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+    }
+
+    @Test
+    public void create_user_happy_path() {
+        when(encoder.encode("testtest")).thenReturn("thisIsHashed");
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("Sina");
+        r.setPassword("12345678");
+        r.setConfirmPassword("12345678");
+
+        ResponseEntity<User> response = userController.createUser(r);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = response.getBody();
+        assertNotNull(user);
+
+        assertEquals(0, user.getId());
+        assertEquals("Sina", user.getUsername());
+    }
+}
